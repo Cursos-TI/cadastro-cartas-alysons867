bef12a0cf0be5638075023ddfa14192245077ae6
alysons867 <alysons867@gmail.com>
2025-09-29 19:54:26 +0000

    Desenvolvendo a Lógica do Jogo - desafio novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
+// Estrutura que representa uma carta do Super Trunfo
 struct Carta {
     char estado;
     char codigo[4];
@@ -12,14 +13,15 @@ struct Carta {
 
     float densidadePopulacional;
     float pibPerCapita;
-    float superPoder;
 };
 
+// Limpa o buffer de entrada
 void limparBuffer() {
     int c;
     while ((c = getchar()) != '\n' && c != EOF) {}
 }
 
+// Lê os dados da carta com scanf/fgets
 void cadastrarCarta(struct Carta *carta) {
     printf("Digite a letra do Estado (A a H): ");
     scanf(" %c", &carta->estado);
@@ -46,6 +48,7 @@ void cadastrarCarta(struct Carta *carta) {
     scanf("%d", &carta->pontosTuristicos);
 }
 
+// Calcula densidade = população / área
 void calcularDensidadePopulacional(struct Carta *carta) {
     if (carta->area > 0)
         carta->densidadePopulacional = carta->populacao / carta->area;
@@ -53,6 +56,7 @@ void calcularDensidadePopulacional(struct Carta *carta) {
         carta->densidadePopulacional = 0.0;
 }
 
+// Calcula PIB per capita = (PIB * 1 bilhão) / população
 void calcularPibPerCapita(struct Carta *carta) {
     if (carta->populacao > 0)
         carta->pibPerCapita = (carta->pib * 1000000000.0) / carta->populacao;
@@ -60,64 +64,56 @@ void calcularPibPerCapita(struct Carta *carta) {
         carta->pibPerCapita = 0.0;
 }
 
-void calcularSuperPoder(struct Carta *carta) {
-    float inversoDensidade = (carta->densidadePopulacional > 0) ? (1.0 / carta->densidadePopulacional) : 0.0;
-
-    carta->superPoder = 
-        (float)carta->populacao +
-        carta->area +
-        carta->pib +  // PIB já está em bilhões
-        (float)carta->pontosTuristicos +
-        carta->pibPerCapita +
-        inversoDensidade;
-}
-
+// Exibe os dados de uma carta
 void exibirCarta(struct Carta carta, int numero) {
-    printf("\n-----------------------------\n");
-    printf("Carta %d:\n", numero);
+    printf("\nCarta %d:\n", numero);
     printf("Estado: %c\n", carta.estado);
     printf("Código: %s\n", carta.codigo);
-    printf("Nome da Cidade: %s\n", carta.nomeCidade);
+    printf("Cidade: %s\n", carta.nomeCidade);
     printf("População: %lu\n", carta.populacao);
     printf("Área: %.2f km²\n", carta.area);
     printf("PIB: %.2f bilhões de reais\n", carta.pib);
     printf("Pontos Turísticos: %d\n", carta.pontosTuristicos);
     printf("Densidade Populacional: %.2f hab/km²\n", carta.densidadePopulacional);
     printf("PIB per Capita: %.2f reais\n", carta.pibPerCapita);
-    printf("Super Poder: %.2f\n", carta.superPoder);
 }
 
-void compararCartas(struct Carta c1, struct Carta c2) {
-    printf("\nComparação de Cartas:\n");
+// Função que compara duas cartas com base em um único atributo
+void compararPorPopulacao(struct Carta c1, struct Carta c2) {
+    printf("\nComparação de cartas (Atributo: População):\n\n");
+    printf("Carta 1 - %s (%c): %lu habitantes\n", c1.nomeCidade, c1.estado, c1.populacao);
+    printf("Carta 2 - %s (%c): %lu habitantes\n", c2.nomeCidade, c2.estado, c2.populacao);
 
-    printf("População: Carta 1 venceu (%d)\n", c1.populacao > c2.populacao);
-    printf("Área: Carta 1 venceu (%d)\n", c1.area > c2.area);
-    printf("PIB: Carta 1 venceu (%d)\n", c1.pib > c2.pib);
-    printf("Pontos Turísticos: Carta 1 venceu (%d)\n", c1.pontosTuristicos > c2.pontosTuristicos);
-    printf("Densidade Populacional: Carta 1 venceu (%d)\n", c1.densidadePopulacional < c2.densidadePopulacional);
-    printf("PIB per Capita: Carta 1 venceu (%d)\n", c1.pibPerCapita > c2.pibPerCapita);
-    printf("Super Poder: Carta 1 venceu (%d)\n", c1.superPoder > c2.superPoder);
+    // Compara populações
+    if (c1.populacao > c2.populacao) {
+        printf("\nResultado: Carta 1 (%s) venceu!\n", c1.nomeCidade);
+    } else if (c2.populacao > c1.populacao) {
+        printf("\nResultado: Carta 2 (%s) venceu!\n", c2.nomeCidade);
+    } else {
+        printf("\nResultado: Empate! Ambas têm a mesma população.\n");
+    }
 }
 
 int main() {
     struct Carta carta1, carta2;
 
+    // Cadastro das cartas
     printf("Cadastro da Carta 1:\n");
     cadastrarCarta(&carta1);
     calcularDensidadePopulacional(&carta1);
     calcularPibPerCapita(&carta1);
-    calcularSuperPoder(&carta1);
 
     printf("\nCadastro da Carta 2:\n");
     cadastrarCarta(&carta2);
     calcularDensidadePopulacional(&carta2);
     calcularPibPerCapita(&carta2);
-    calcularSuperPoder(&carta2);
 
+    // Exibição dos dados
     exibirCarta(carta1, 1);
     exibirCarta(carta2, 2);
 
-    compararCartas(carta1, carta2);
+    // Comparar por um único atributo (População, neste exemplo)
+    compararPorPopulacao(carta1, carta2);
 
     return 0;
 }
